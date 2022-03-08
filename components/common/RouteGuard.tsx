c7c3b8a6830f7c5062ae977c0141e1e5608ebe01
Sarah <asarahali00@gmail.com>
2022-03-07 21:02:24 -0600

    update spinner for loading auth

--- components/common/RouteGuard.tsx
+++ components/common/RouteGuard.tsx
@@ -9,6 +9,19 @@ function RouteGuard({ children }: { children: ReactNode}) {
   const { user } = useContext(AuthContext);
 
   if (user === null) return <Login />;
+  if (user === undefined) {
+    return (
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+      >
+        <Spin spinning size="large" />
+      </div>
+    );
+  }
   return <Spin spinning={user === undefined}>{children}</Spin>;
 }
 
