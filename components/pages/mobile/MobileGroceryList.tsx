a953ab61bdaf84313edcc4a50ac9ed55d6288a59
sarahannali <55365172+sarahannali@users.noreply.github.com>
2022-03-19 16:52:55 -0500

    Add mobile grocery list (#3)
    * add mobile grocery list
    
    * add logout button to mobile screen
    
    * fix login background ui

---
+++ components/pages/mobile/MobileGroceryList.tsx
@@ -0,0 +1,79 @@
+import {
+  Button, Row, Spin, Typography,
+} from 'antd';
+import React, { useContext, useEffect, useState } from 'react';
+import Image from 'next/image';
+import { signOut } from 'firebase/auth';
+import { GroceryItem } from '../../../utils/propTypes';
+import { getGroceryList, postGroceryList } from '../../requests';
+import GroceryList from '../mealplanner/components/groceryList/GroceryList';
+import classes from '../login/login.module.css';
+import { AuthContext } from '../../contexts/AuthContext';
+import { auth } from '../../../firebase/clientApp';
+
+const { Title } = Typography;
+
+function MobileGroceryList() {
+  const [groceryList, setGroceryList] = useState<GroceryItem[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const { setUser } = useContext(AuthContext);
+
+  const handleLogOut = (): void => {
+    signOut(auth).then(() => {
+      if (setUser) setUser(null);
+    });
+  };
+
+  const updateAndPostGroceryList = (updatedGroceryList: GroceryItem[]) => {
+    setGroceryList(updatedGroceryList);
+    postGroceryList(updatedGroceryList);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    getGroceryList()
+      .then((res) => {
+        setGroceryList(res);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div style={{
+      height: '100vh', overflow: 'hidden', position: 'relative', backgroundColor: '#f2f9ff',
+    }}
+    >
+      <Spin spinning={loading} size="large" style={{ marginTop: '170px' }}>
+        <div className={classes.fullScreen}>
+          <div className={classes.loginModal} style={{ overflowY: 'scroll', width: '80%', height: '80%' }}>
+            <Row justify="center" style={{ marginBottom: '20px' }}>
+              <Image src="/images/Icon.png" height={80} width={80} />
+            </Row>
+            <Row justify="center" style={{ marginBottom: '20px' }}>
+              <Title level={2}>GROCERY LIST</Title>
+            </Row>
+            <GroceryList
+              groceryList={groceryList}
+              updateAndPostGroceryList={updateAndPostGroceryList}
+            />
+          </div>
+          <Button
+            danger
+            type="primary"
+            onClick={handleLogOut}
+            style={{
+              position: 'absolute',
+              bottom: '15px',
+            }}
+          >
+            Logout
+          </Button>
+        </div>
+      </Spin>
+    </div>
+  );
+}
+
+export default MobileGroceryList;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import '../styles.css';
 import AuthProvider from '../components/contexts/AuthContext';
 import RouteGuard from '../components/common/RouteGuard';
+import MobileGroceryList from '../components/pages/mobile/MobileGroceryList';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -17,11 +18,26 @@ type AppPropsWithLayout = AppProps & {
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
+  const [width, setWidth] = useState<number>();
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
 
   return (
     <AuthProvider>
       <RouteGuard>
-        {getLayout(<Component {...pageProps} />)}
+        { width && width <= 768
+          ? <MobileGroceryList />
+          : getLayout(<Component {...pageProps} />)}
       </RouteGuard>
     </AuthProvider>
   );
