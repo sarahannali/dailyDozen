d6705fdf2f61dcdbd8f3bef38d1e2d13ebc64b6d
Sarah <asarahali00@gmail.com>
2022-03-26 20:20:28 -0500

    fix guard misspelling

--- components/common/components/RouteGuard/RouteGuard.tsx
+++ components/common/components/RouteGuard/RouteGuard.tsx
@@ -4,7 +4,7 @@ import React, {
 } from 'react';
 import { AuthContext } from 'components/contexts/AuthContext';
 import Login from 'components/pages/login';
-import classes from 'components/css/routeGurad.module.css';
+import classes from 'components/css/routeGuard.module.css';
 
 function RouteGuard({ children }: { children: ReactNode}) {
   const { user } = useContext(AuthContext);
